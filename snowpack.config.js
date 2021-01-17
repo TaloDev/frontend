731f98cfe238899ab3eb930b3a3230bdc9875c6e
Tudor <sekaru@sleepystudios.net>
2021-01-17 14:09:09 +0000

    use webpack bundler

--- snowpack.config.js
+++ snowpack.config.js
@@ -6,24 +6,18 @@ module.exports = {
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-postcss'
+    '@snowpack/plugin-postcss',
+    '@snowpack/plugin-webpack'
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
-  optimize: {
-    bundle: true,
-    minify: true,
-    target: 'esnext'
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    baseUrl: '/gs/'
-  },
+  // optimize: {
+  //   bundle: true,
+  //   minify: true,
+  //   target: 'es2020'
+  // },
+  packageOptions: {},
+  devOptions: {}
 }
