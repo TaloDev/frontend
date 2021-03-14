b1c573e574dba5288c154876e153390c9405e847
Tudor <sekaru@sleepystudios.net>
2021-03-14 11:46:39 +0000

    missed it

---
+++ src/components/ChartTooltip.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { format } from 'date-fns'
+import uniqBy from 'lodash.uniqby'
+import randomColor from 'randomcolor'
+
+const ChartTooltip = (props) => {
+  const filteredItems = props.payload?.filter((item) => item.payload.count > 0)
+
+  if (!props.active || filteredItems?.length === 0) return null
+
+  return (
+    <div className='bg-white p-4 rounded'>
+      <p className='text-black font-medium'>{format(new Date(props.label), 'do MMM yyyy')}</p>
+      <ul>
+        {uniqBy(filteredItems, 'payload.name').map((item, idx) => (
+          <li key={idx}>
+            <p className='text-black text-md'>
+              <span className='mr-2 w-4 h-4 rounded inline-block align-text-bottom' style={{ backgroundColor: randomColor({ seed: item.payload.name }) }} />
+              {item.payload.name}: {item.payload.count}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+ChartTooltip.propTypes = {
+  active: PropTypes.bool,
+  payload: PropTypes.array,
+  label: PropTypes.string
+}
+
+export default ChartTooltip
