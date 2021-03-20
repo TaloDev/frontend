d666fc4a824e23863740e9ac1083849310f5d627
Tudor <sekaru@sleepystudios.net>
2021-03-20 11:02:07 +0000

    add percent change in tooltip

--- src/components/charts/ChartTooltip.jsx
+++ src/components/charts/ChartTooltip.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { format } from 'date-fns'
 import uniqBy from 'lodash.uniqby'
 import randomColor from 'randomcolor'
+import classNames from 'classnames'
 
 const ChartTooltip = (props) => {
   const filteredItems = props.payload?.filter((item) => item.payload.count > 0)
@@ -11,15 +12,26 @@ const ChartTooltip = (props) => {
 
   return (
     <div className='bg-white p-4 rounded'>
-      <p className='text-black font-medium mb-2'>{format(new Date(props.label), 'do MMM yyyy')}</p>
-      <ul>
+      <p className='text-black font-medium mb-2'>{format(new Date(props.label), 'EEEE do MMM yyyy')}</p>
+      <ul className='space-y-2'>
         {uniqBy(filteredItems, 'payload.name')
           .sort((a, b) => b.payload.count - a.payload.count)
           .map((item, idx) => (
             <li key={idx}>
-              <p className='text-black text-md'>
+              <p className='text-black text-sm'>
                 <span className='mr-2 w-4 h-4 rounded inline-block align-text-bottom' style={{ backgroundColor: randomColor({ seed: item.payload.name }) }} />
                 {item.payload.name}: {item.payload.count}
+
+                <span className={classNames(
+                  'p-1 rounded ml-2 text-xs',
+                  {
+                    'bg-red-100': item.payload.change < 0,
+                    'bg-green-100': item.payload.change > 0,
+                    'bg-gray-100': item.payload.change === 0
+                  }
+                )}>
+                  {(item.payload.change * 100).toFixed(1)}%
+                </span>
               </p>
             </li>
           ))}
