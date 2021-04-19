d65cef76746f0e65fef2b894c923057cdaac3dce
Tudor <sekaru@sleepystudios.net>
2021-04-19 22:48:28 +0100

    player aliases as list

--- src/components/PlayerAliases.jsx
+++ src/components/PlayerAliases.jsx
@@ -4,20 +4,20 @@ import { IconBrandSteam, IconQuestionMark } from '@tabler/icons'
 
 const PlayerAliases = (props) => {
   const getIcon = (alias) => {
-    switch (alias) {
+    switch (alias.service) {
       case 'steam': return <IconBrandSteam size={16} />
       default: return <IconQuestionMark size={16} />
     }
   }
 
-  if (!props.aliases || Object.keys(props.aliases).length === 0) return 'None'
+  if (!props.aliases || props.aliases.length === 0) return 'None'
 
   return (
     <div className='space-y-2'>
-      {Object.keys(props.aliases).map((alias) => (
+      {props.aliases.map((alias) => (
         <div key={alias} className='flex items-center'>
           <span className='p-1 rounded-full bg-gray-900'>{getIcon(alias)}</span>
-          <span className='ml-2 text-sm'>{props.aliases[alias]}</span>
+          <span className='ml-2 text-sm'>{alias.identifier}</span>
         </div>
       ))}
     </div>
