ca54f4d7001fefaccada9838398a5105ca0d24af
Tudor <sekaru@sleepystudios.net>
2021-05-18 23:05:36 +0100

    persist active game state in localstorage

--- src/state/activeGameState.js
+++ src/state/activeGameState.js
@@ -2,7 +2,14 @@ import { atom } from 'recoil'
 
 const activeGameState = atom({
   key: 'activeGame',
-  default: null
+  default: JSON.parse(window.localStorage.getItem('activeGame')),
+  effects_UNSTABLE: [
+    ({ onSet }) => {
+      onSet((updatedActiveGame) => {
+        window.localStorage.setItem('activeGame', JSON.stringify(updatedActiveGame))
+      })
+    }
+  ]
 })
 
 export default activeGameState
