0c47a8ebff4ffe1f347f4023d2507be8f7743259
Tudor <sekaru@sleepystudios.net>
2021-05-26 22:42:14 +0100

    add css nesting and tippy theme css

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
+import 'tippy.js/dist/tippy.css'
 import App from './App'
 import { RecoilRoot } from 'recoil'
 import { BrowserRouter } from 'react-router-dom'
