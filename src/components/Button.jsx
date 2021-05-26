10fd785fe83aa5e426a4f9ebffecd8a8e0933199
Tudor <sekaru@sleepystudios.net>
2021-05-26 22:42:25 +0100

    black button variant

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,6 +5,8 @@ import Loading from './Loading'
 import classNames from 'classnames'
 
 const Button = (props) => {
+  const colourVariants = ['grey', 'red', 'green', 'black']
+
   const className = classNames(`
     disabled:opacity-40
     disabled:cursor-not-allowed
@@ -12,11 +14,12 @@ const Button = (props) => {
     ${focusStyle}
     ${props.className ?? ''}
   `, {
-    'bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-800 disabled:bg-indigo-500 text-white': !props.variant,
-    'px-4 py-2 w-full rounded font-semibold': [undefined, 'grey', 'red', 'green'].includes(props.variant),
+    'bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-800 disabled:bg-indigo-500 text-white': !props.variant, // default purple button
+    'px-4 py-2 w-full rounded font-semibold': [undefined, ...colourVariants].includes(props.variant),
     'bg-gray-200 hover:bg-gray-300 active:bg-gray-400 disabled:bg-gray-200 text-black': props.variant === 'grey',
     'bg-red-600 hover:bg-red-700 active:bg-red-800 disabled:bg-red-600 text-white': props.variant === 'red',
     'bg-green-600 hover:bg-green-700 active:bg-green-800 disabled:bg-green-600 text-white': props.variant === 'green',
+    'bg-gray-800 hover:bg-gray-900 active:bg-black disabled:bg-gray-600 text-white': props.variant === 'black',
     'text-left': props.variant === 'bare',
     'flex justify-center hover:bg-indigo-500': props.isLoading,
     'flex items-center space-x-1': Boolean(props.icon)
