8379d2a4beb97b8ffc571833bded534887fc16bc
Tudor <sekaru@sleepystudios.net>
2021-06-10 20:29:26 +0100

    mobile nav

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import { IconArrowLeft } from '@tabler/icons'
+import Button from './Button'
+import { useLocation } from 'react-router'
+
+const MobileMenu = (props) => {
+  const location = useLocation()
+  const [pathname, setPathname] = useState(location.pathname)
+
+  useEffect(() => {
+    setPathname(location.pathname)
+  }, [props.visible])
+
+  useEffect(() => {
+    if (props.visible && location.pathname !== pathname) {
+      props.onClose()
+    }
+  }, [location.pathname, pathname, props.visible])
+
+  return (
+    <div className={classNames(
+      'fixed transition-transform transform top-0 left-0 bg-gray-900 p-8 w-full h-full z-[999] ',
+      {
+        'translate-x-0': props.visible,
+        '-translate-x-full': !props.visible
+      }
+    )}>
+        <Button
+          variant='icon'
+          onClick={props.onClose}
+        >
+          <span className='text-white bg-indigo-600 rounded-full p-1 flex'>
+            <IconArrowLeft size={36} />
+          </span>
+        </Button>
+
+      <ul className='mt-10 space-y-10 text-2xl'>
+        {props.children}
+      </ul>
+    </div>
+  )
+}
+
+MobileMenu.propTypes = {
+  children: PropTypes.node.isRequired,
+  visible: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired
+}
+
+export default MobileMenu
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRecoilValue } from 'recoil'
 import logout from '../api/logout'
 import LinkButton from './LinkButton'
@@ -8,10 +8,14 @@ import GameSwitcher from './GameSwitcher'
 import activeGameState from '../state/activeGameState'
 import userState from '../state/userState'
 import userTypes from '../constants/userTypes'
+import { IconMenu2 } from '@tabler/icons'
+import MobileMenu from './MobileMenu'
+import Button from './Button'
 
 const NavBar = () => {
   const activeGame = useRecoilValue(activeGameState)
   const user = useRecoilValue(userState)
+  const [showMobileMenu, setShowMobileMenu] = useState(false)
 
   const onLogoutClick = async () => {
     try {
@@ -23,31 +27,50 @@ const NavBar = () => {
     }
   }
 
+  const links = (
+    <>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      {activeGame &&
+        <>
+          <li>
+            <Link to={routes.players}>Players</Link>
+          </li>
+          <li>
+            <Link to={routes.events}>Events</Link>
+          </li>
+          {user.type === userTypes.ADMIN &&
+            <li>
+              <Link to={routes.apiKeys}>Access keys</Link>
+            </li>
+          }
+        </>
+      }
+      <li>
+        <LinkButton onClick={onLogoutClick}>Logout</LinkButton>
+      </li>
+    </>
+  )
+
   return (
-    <nav className='bg-gray-900 w-full p-4 md:p-8 flex justify-between'>
-      <ul className='hidden md:flex space-x-4 md:space-x-8 items-center'>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        {activeGame &&
-          <>
-            <li>
-              <Link to={routes.players}>Players</Link>
-            </li>
-            <li>
-              <Link to={routes.events}>Events</Link>
-            </li>
-            {user.type === userTypes.ADMIN &&
-              <li>
-                <Link to={routes.apiKeys}>Access keys</Link>
-              </li>
-            }
-          </>
-        }
-        <li>
-          <LinkButton onClick={onLogoutClick}>Logout</LinkButton>
-        </li>
+    <nav className='bg-gray-900 w-full p-4 md:p-8 flex justify-between items-center'>
+      <ul className='hidden md:flex space-x-4 md:space-x-8'>
+        {links}
       </ul>
+
+      <div className='md:hidden text-white'>
+        <Button
+          variant='icon'
+          onClick={() => setShowMobileMenu(true)}
+        >
+          <IconMenu2 size={24} />
+        </Button>
+      </div>
+
+      <MobileMenu visible={showMobileMenu} onClose={() => setShowMobileMenu(false)}>
+        {links}
+      </MobileMenu>
       
       <GameSwitcher />
     </nav>
