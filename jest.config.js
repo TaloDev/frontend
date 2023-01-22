7af76f0cbf9bac79debc729c7cdeb4c53572f7dd
tudor <7089284+tudddorrr@users.noreply.github.com>
2023-01-22 14:43:01 +0000

    remove jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-module.exports = {
-  collectCoverage: true,
-  coverageReporters: ['lcov'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/*/api/*/', '/*/constants/*/'],
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/(?!lodash-es)'
-  ]
-}
