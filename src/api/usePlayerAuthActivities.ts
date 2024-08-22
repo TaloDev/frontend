e13569e4021879fc5962eda0c51e9f24c7a45109
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-08-22 13:36:02 +0100

    fix 403 when trying to get auth activities as a non-admin

--- src/api/usePlayerAuthActivities.ts
+++ src/api/usePlayerAuthActivities.ts
@@ -4,8 +4,10 @@ import { Game } from '../entities/game'
 import makeValidatedGetRequest from './makeValidatedGetRequest'
 import { z } from 'zod'
 import { playerAuthActivitySchema } from '../entities/playerAuthActivity'
+import canPerformAction, { PermissionBasedAction } from '../utils/canPerformAction'
+import { AuthedUser } from '../state/userState'
 
-export default function usePlayerAuthActivities(activeGame: Game, playerId: string) {
+export default function usePlayerAuthActivities(activeGame: Game, playerId: string, user: AuthedUser) {
   const fetcher = async ([url]: [string]) => {
     const res = await makeValidatedGetRequest(url, z.object({
       activities: z.array(playerAuthActivitySchema)
@@ -15,7 +17,9 @@ export default function usePlayerAuthActivities(activeGame: Game, playerId: stri
   }
 
   const { data, error } = useSWR(
-    [`/games/${activeGame.id}/players/${playerId}/auth-activities`],
+    canPerformAction(user, PermissionBasedAction.VIEW_PLAYER_AUTH_ACTIVITIES)
+      ? [`/games/${activeGame.id}/players/${playerId}/auth-activities`]
+      : null,
     fetcher
   )
 
