bca8571c1847c750befb6bb76a304f700541fae3
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-11-01 22:54:40 +0000

    allow updating game names

--- src/api/updateGame.ts
+++ src/api/updateGame.ts
@@ -5,7 +5,7 @@ import makeValidatedRequest from './makeValidatedRequest'
 import { Prop } from '../entities/prop'
 
 const updateGame = makeValidatedRequest(
-  (gameId: number, data: { props: Prop[] }) => api.patch(`/games/${gameId}`, data),
+  (gameId: number, data: { name?: string, props?: Prop[] }) => api.patch(`/games/${gameId}`, data),
   z.object({
     game: gameSchema
   })
