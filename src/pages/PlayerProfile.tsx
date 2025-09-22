e288e3d64e5d057681205accc896f25dd9102d81
tudor <7089284+tudddorrr@users.noreply.github.com>
2025-09-22 22:05:19 +0100

    add player deleting

--- src/pages/PlayerProfile.tsx
+++ src/pages/PlayerProfile.tsx
@@ -12,7 +12,7 @@ import Table from '../components/tables/Table'
 import SecondaryTitle from '../components/SecondaryTitle'
 import PlayerAliases from '../components/PlayerAliases'
 import Identifier from '../components/Identifier'
-import { IconBolt, IconChartBar, IconDeviceFloppy, IconSettings, IconTrophy } from '@tabler/icons-react'
+import { IconBolt, IconChartBar, IconDeviceFloppy, IconSettings, IconTrash, IconTrophy } from '@tabler/icons-react'
 import Button from '../components/Button'
 import Loading from '../components/Loading'
 import usePlayerAuthActivities from '../api/usePlayerAuthActivities'
@@ -22,6 +22,11 @@ import useDaySections from '../utils/useDaySections'
 import ActivityRenderer from '../components/ActivityRenderer'
 import userState, { AuthedUser } from '../state/userState'
 import clsx from 'clsx'
+import Tile from '../components/Tile'
+import { useCallback, useContext, useState } from 'react'
+import ToastContext, { ToastType } from '../components/toast/ToastContext'
+import deletePlayer from '../api/deletePlayer'
+import canPerformAction, { PermissionBasedAction } from '../utils/canPerformAction'
 
 const links = [
   {
@@ -63,13 +68,35 @@ export default function PlayerProfile() {
 
   const sections = useDaySections(activities)
 
-  const goToPlayerRoute = (route: string) => {
+  const [isDeleting, setIsDeleting] = useState(false)
+  const toast = useContext(ToastContext)
+
+  const goToPlayerRoute = useCallback((route: string) => {
     if (!player) {
       throw new Error('Player not found')
     }
 
     navigate(route.replace(':id', player.id))
-  }
+  }, [navigate, player])
+
+  const onDeleteClick = useCallback(async () => {
+    if (window.confirm('Are you sure you want to delete this player? This action cannot be undone.')) {
+      setIsDeleting(true)
+      try {
+        await deletePlayer(activeGame.id, player!.id)
+      } catch (err) {
+        setIsDeleting(false)
+        toast.trigger('Something went wrong while deleting the player', ToastType.ERROR)
+        return
+      }
+
+      toast.trigger('Player deleted')
+
+      setTimeout(() => {
+        navigate(routes.players)
+      }, 2000)
+    }
+  }, [activeGame.id, navigate, player, toast])
 
   if (!player) {
     return (
@@ -136,6 +163,36 @@ export default function PlayerProfile() {
           {sections.map((section, sectionIdx) => (
             <ActivityRenderer key={sectionIdx} section={section} />
           ))}
+        </>
+      }
+
+      {canPerformAction(user, PermissionBasedAction.DELETE_PLAYER) &&
+        <>
+          <SecondaryTitle>Danger zone</SecondaryTitle>
+          <div className='p-4 border-2 border-dashed border-red-400 rounded w-full lg:w-3/4 xl:w-1/2'>
+            <Tile
+              header={(
+                <h2 className='text-xl font-semibold'>Delete player</h2>
+              )}
+              content={(
+                <div className='sm:flex items-center justify-between sm:space-x-4 space-y-4 sm:space-y-0'>
+                  <p className='sm:w-3/4'>This will remove all data associated with this player and they will no longer have access to their account. <span className='font-semibold'>This action cannot be undone</span>.</p>
+                  <Button
+                    type='button'
+                    isLoading={isDeleting}
+                    onClick={onDeleteClick}
+                    variant='red'
+                    className='!w-auto'
+                    icon={<IconTrash />}
+                  >
+                    <span>
+                      Delete
+                    </span>
+                  </Button>
+                </div>
+              )}
+            />
+          </div>
         </>
       }
     </Page>
