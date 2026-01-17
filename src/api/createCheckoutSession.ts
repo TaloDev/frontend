07860f2da67e8de14c845d3ba9c24b2228ed0054
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-01-17 13:19:31 +0000

    add typing to createCheckoutSession

--- src/api/createCheckoutSession.ts
+++ src/api/createCheckoutSession.ts
@@ -4,7 +4,7 @@ import makeValidatedRequest from './makeValidatedRequest'
 import { invoiceSchema } from '../entities/invoice'
 
 const createCheckoutSession = makeValidatedRequest(
-  (pricingPlanId, pricingInterval) => api.post('/billing/checkout-session', { pricingPlanId, pricingInterval }),
+  (pricingPlanId: number, pricingInterval: string) => api.post('/billing/checkout-session', { pricingPlanId, pricingInterval }),
   z.object({
     redirect: z.string().url().optional(),
     invoice: invoiceSchema.optional()
