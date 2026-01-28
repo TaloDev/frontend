b3ff774a2e977954db22822ed15c767cf2a30e3b
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-01-28 23:11:05 +0000

    live config toast

--- src/pages/GameProps.tsx
+++ src/pages/GameProps.tsx
@@ -4,12 +4,19 @@ import { useRecoilState } from 'recoil'
 import PropsEditor from '../components/PropsEditor'
 import updateGame from '../api/updateGame'
 import { Prop } from '../entities/prop'
+import { useContext } from 'react'
+import ToastContext, { ToastType } from '../components/toast/ToastContext'
 
 export default function GameProps() {
   const [activeGame, setActiveGame] = useRecoilState(activeGameState) as SelectedActiveGameState
 
+  const toast = useContext(ToastContext)
+
   const onSave = async (props: Prop[]): Promise<Prop[]> => {
     const { game } = await updateGame(activeGame.id, { props })
+
+    toast.trigger('Live config updated', ToastType.SUCCESS)
+
     setActiveGame(game)
     return game.props
   }
