a195af3bf18b058c06ef8075484a2b3bf3857670
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-02-19 21:30:16 +0000

    loading state for charts

--- src/components/charts/ChartCard.tsx
+++ src/components/charts/ChartCard.tsx
@@ -5,6 +5,7 @@ import TimePeriodPicker from '../TimePeriodPicker'
 import { timePeriods } from '../../utils/useTimePeriodAndDates'
 import { TimePeriod } from '../../utils/useTimePeriod'
 import { LabelledTimePeriod } from '../TimePeriodPicker'
+import Loading from '../Loading'
 
 type ChartCardProps = {
   title: string
@@ -32,7 +33,14 @@ export function ChartCard({
   return (
     <div className='hidden md:block border-2 border-gray-700 rounded bg-black space-y-8 p-4'>
       <div className='flex items-start justify-between'>
-        <h2 className='text-xl'>{title}</h2>
+        <div className='flex gap-4'>
+          <h2 className='text-xl'>{title}</h2>
+          {loading &&
+            <div className='mt-1'>
+              <Loading size={24} thickness={180} />
+            </div>
+          }
+        </div>
         {onTimePeriodChange &&
           <TimePeriodPicker
             periods={timePeriods}
