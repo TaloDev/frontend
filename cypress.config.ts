cce74ba7eb508d72c6af22aadb5603181be7b30f
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-02-28 00:55:57 +0000

    remove unneeded cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
-    setupNodeEvents(_on, _config) {
-      // implement node event listeners here
-    },
   },
 })
